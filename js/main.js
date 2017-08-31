5f0ebc5d33357b1e164624c758ac98bd10222b8e
Alexander Bogish <ego.cogitare@gmail.com>
2017-08-31 06:35:29 -0700

    ...

--- js/main.js
+++ js/main.js
@@ -24,8 +24,19 @@ $(document).ready(function() {
     speed: 1000
   });
 
+  $('.menu .menu-item A').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).closest('.menu').find('A').removeClass('active');
+    $(this).addClass('active');
+    var id = $(this).attr('href');
+    $('html, body').animate({
+       scrollTop: $(id).offset().top - 74
+    }, 1000);
+  });
+
   $('.product-thumb').on('click', function() {
-    var $productImg = $(this).parent().next('.product-photo').find('IMG');
+    var $productImg = $(this).parent().prev('.product-photo').find('IMG');
     $productImg.attr('src', $(this).find('IMG').attr('src'));
     $(this).addClass('active').siblings('.product-thumb').removeClass('active');
   });
